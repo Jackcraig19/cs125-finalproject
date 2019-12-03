1bc2717538e72cbc83654031ecc91e97c32280f4
InigoK <ipk2@illinois.edu>
2019-12-02 21:08:01 -0600

    commit3

--- app/src/main/java/com/example/cs125_finalproject/NewContactActivity.java
+++ app/src/main/java/com/example/cs125_finalproject/NewContactActivity.java
@@ -14,7 +14,7 @@ public class NewContactActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_contact);
         final NewContactActivity context = this;
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
 
         Button returnButton = findViewById(R.id.returnButton);
         returnButton.setOnClickListener(new View.OnClickListener() {
@@ -28,7 +28,6 @@ public class NewContactActivity extends AppCompatActivity {
         addContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 startActivity(new Intent(context, MainActivity.class));
                 finish();
             }
