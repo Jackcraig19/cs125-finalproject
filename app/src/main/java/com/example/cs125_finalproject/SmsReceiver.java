9cc8861853ada028fc2922207090b58b8e6dc99e
Craig <jvcraig2@illinois.edu>
2019-12-03 15:48:33 -0600

    Added receive messages functionality

--- app/src/main/java/com/example/cs125_finalproject/SmsReceiver.java
+++ app/src/main/java/com/example/cs125_finalproject/SmsReceiver.java
@@ -5,6 +5,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.lang.annotation.Target;
 
@@ -14,6 +18,25 @@ public class SmsReceiver extends BroadcastReceiver {
     @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs;
+        String strMessage = "";
+        String format = bundle.getString("format");
+        Object[] pdus = (Object[]) bundle.get(pdu_type);
+        if (pdus != null) {
+            msgs = new SmsMessage[pdus.length];
+            boolean isVersionM = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
+            for (int i = 0; i < pdus.length; i++) {
+                if (isVersionM) {
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                } else {
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+                strMessage += "SMS from " + msgs[i].getOriginatingAddress();
+                strMessage += " :" + msgs[i].getMessageBody() + "\n";
+                Log.d(TAG, "onReceive: " + strMessage);
+                Toast.makeText(context, strMessage, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
