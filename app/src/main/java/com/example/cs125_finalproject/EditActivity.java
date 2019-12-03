0c81dd63fee133a46cb6be04eff9e82d685fb003
Jack Craig <jackdelcraig@gmail.com>
2019-12-02 22:34:47 -0600

    made Handler class and editlist get filled

--- app/src/main/java/com/example/cs125_finalproject/EditActivity.java
+++ app/src/main/java/com/example/cs125_finalproject/EditActivity.java
@@ -2,12 +2,16 @@ package com.example.cs125_finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -24,15 +28,28 @@ public class EditActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(context, MainActivity.class));
+
                 finish();
             }
         });
+        fillList();
+    }
+
+    private void fillList() {
         LinearLayout parent = findViewById(R.id.contactLayout);
-        View testChunk = getLayoutInflater().inflate(R.layout.chunk_contact, parent, false);
-        TextView nameView = testChunk.findViewById(R.id.name);
-        nameView.setText("Test Name");
-        TextView numberView = testChunk.findViewById(R.id.number);
-        numberView.setText("(934)-934-0189");
-        parent.addView(testChunk);
+        parent.removeAllViews();
+        ArrayList<Contact> contacts = Handler.contacts;
+        for (Contact c : contacts) {
+            Log.d("EditActivityDebug","New Contact");
+            View testChunk = getLayoutInflater().inflate(R.layout.chunk_contact, parent, false);
+            TextView nameView = testChunk.findViewById(R.id.name);
+            nameView.setText(c.getName());
+            TextView numberView = testChunk.findViewById(R.id.number);
+            numberView.setText(String.valueOf(c.getNumber()));
+            Switch toggleSwitch = testChunk.findViewById(R.id.toggle);
+            toggleSwitch.setChecked(c.getState());
+            parent.addView(testChunk);
+        }
+
     }
 }
