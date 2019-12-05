63f0973f9395efe07899400a579dc3a33598bab9
InigoK <ipk2@illinois.edu>
2019-12-05 16:21:18 -0600

    MessageBuilder

--- app/src/main/java/com/example/cs125_finalproject/SmsReceiver.java
+++ app/src/main/java/com/example/cs125_finalproject/SmsReceiver.java
@@ -9,29 +9,10 @@ import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
-import android.widget.Toast;
-
-import java.lang.annotation.Target;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SmsReceiver extends BroadcastReceiver {
     private static final String TAG = SmsReceiver.class.getSimpleName();
     public static final String pdu_type = "pdus";
-    private static Map<String, String[]> responseMap = new HashMap<>();
-
-    public static void setupMessages() {
-        String[] greetingsList = {"hello", "hi", "howdy", "greetings"};
-        for (String s1 : greetingsList)
-            responseMap.put(s1, greetingsList);
-        String[] salutationsList = {"bye", "goodbye", "peace", "adios"};
-        for (String s1 : salutationsList)
-            responseMap.put(s1, salutationsList);
-        String[] catList = {"I love cats", "I hate cats"};
-        responseMap.put("cat", catList);
-        responseMap.put("cats", catList);
-    }
 
     @TargetApi(Build.VERSION_CODES.M)
     @Override
@@ -69,31 +50,11 @@ public class SmsReceiver extends BroadcastReceiver {
     private void sendSMSMessage(String msgToRespond, Contact toSend) {
         try {
             SmsManager smsManager = SmsManager.getDefault();
-            String text = getResponse(msgToRespond) + " (Test)";
+            String text = MessageBuilder.getResponse(msgToRespond, toSend.getName()) + " (Test)";
             smsManager.sendTextMessage(toSend.getNumber(), null, text, null, null);
         } catch (Exception e) {
             Log.e(TAG, "[Error]", e);
             e.printStackTrace();
         }
-    }
-    private String getResponse(String message) {
-        String[] msgArray = message.split(" ");
-        return getResponseHelper(msgArray);
-
-    }
-    private String getResponseHelper(String[] message) {
-        String thisAddition;
-        for (int i = 0; i < message.length; i++) {
-            String[] r = responseMap.get(message[i].toLowerCase().replaceAll("\\W",""));
-            if (r != null) {
-                if (i + 1 < message.length)
-                    message = Arrays.copyOfRange(message,i + 1, message.length);
-                else
-                    message = new String[0];
-                thisAddition = r[(int)(Math.random() * r.length)];
-                return thisAddition + " " + getResponseHelper(message);
-            }
-        }
-        return "";
     }
 }
